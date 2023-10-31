a8dfa40e33fa9a41f9ce5c783346c10fc5e03b4c
Alina Kasiuk <jupiterianw@gmail.com>
2023-10-31 13:41:01 +0100

    Add files via upload

--- husky_motion_test.py
+++ husky_motion_test.py
@@ -8,7 +8,7 @@ import open3d as o3d
 import random
 import time
 from sklearn.preprocessing import normalize
-
+import sys
 
 #import liveplot
 
@@ -54,43 +54,48 @@ if __name__ == '__main__':
             
             if points.size == 0:
             	points = 0
+            	norm = 0
             else:
-            	points = np.round(points, 5)
-            	import numpy as np
+            	#points = np.round(points, 5)
             	
             	#TODO: Normalize the cloud
             	
-            	#norm1 = points / np.linalg.norm(points)
-            	#norm2 = normalize(points[:,np.newaxis], axis=0).ravel()
-            	#print("norm")
-            	#print(np.all(norm1 == norm2))
+            	#max_points = np.array([np.max(np.absolute(points[0])), np.max(np.absolute(points[1])), np.max(np.absolute(points[2]))])
+            	#norm = points / np.max(max_points)
+            	#norm = points / 100
             	
-            	
-            	pcd = o3d.geometry.PointCloud()
-            	pcd.points = o3d.utility.Vector3dVector(points)
-            	voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.001)
+            	#pcd = o3d.geometry.PointCloud()
+            	#pcd.points = o3d.utility.Vector3dVector(norm)
+            	#voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.001)
             
             
             #np.set_printoptions(threshold=sys.maxsize)
             #text_file = open("Points.txt", "w")
             #text_file.write(np.array2string(points))
             #text_file.close()
+            
+            	point_dir = "Points1/Point" + str(x) + "_" + str(i) + ".txt"
+            	points_to_txt = np.savetxt(point_dir, points, fmt="%.3f")
 
 
-            print("action ", actions[i] ," done")
-            print("velocity ", real_vel)
-            print(points)
+            	print("action ", actions[i] ," done")
+            	print("velocity ", real_vel)
+            	print(points)
+            	print("Normalized")
+            	print("___________________")
+            	#print(norm)
+            	#print("___________________")
 
         # Initialize a visualizer object
-        vis = o3d.visualization.Visualizer()
+       # vis = o3d.visualization.Visualizer()
         # Create a window, name it and scale it
-        vis.create_window(window_name='Bunny Visualize', width=800, height=600)
+       # vis.create_window(window_name='Bunny Visualize', width=800, height=600)
         # Add the voxel grid to the visualize
-        vis.add_geometry(voxel_grid)
+       # vis.add_geometry(voxel_grid)
         # We run the visualizater
-        vis.run()
+      #  vis.run()
         # Once the visualizer is closed destroy the window and clean up
-        vis.destroy_window()
+      #  vis.destroy_window()
             
 
 #        if x%100==0:

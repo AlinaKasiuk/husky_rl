4d0b29650ac6f68646f7523abafbf6a6b2d74d1a
Alina Kasiuk <jupiterianw@gmail.com>
2023-11-03 12:39:32 +0100

    Reduce points

--- point_to_voxel.py
+++ point_to_voxel.py
@@ -7,19 +7,20 @@ Created on Wed Oct 18 11:08:22 2023
 import numpy as np
 import open3d as o3d
 
-#point_cloud = np.loadtxt('bunnyStatue.txt', delimiter=' ')
-point_cloud = np.loadtxt('Points.txt', delimiter=' ')
- #points = point_cloud[:,:3]
-points = point_cloud/100
 
-#points = np.random.rand(1000, 3)
-#a = np.reshape(point_cloud[:,5], (point_cloud.shape[0],1))
-#b = np.zeros([point_cloud.shape[0], 2])
-#colors = np.append(b,a, axis=1)
+def observation(filename):
+    point_cloud = np.loadtxt('Points.txt', delimiter=' ')
+    point_cloud = point_cloud[((np.square(point_cloud[:, 0]) + np.square(point_cloud[:, 1])) < 400) &
+                              (np.absolute(point_cloud[:, 2]) < 20)]
+    points = point_cloud/500
+    return points
 
-for i in range (10):
-    new_points = (np.random.rand(1000, 3)-0.5)*2
-    points = np.append(points,new_points, axis=0)
+points = observation('Points.txt')
+new_points = observation('Points\Point0_2.txt')
+
+points = np.append(points,new_points, axis=0)
+
+
 
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(points)
@@ -28,7 +29,7 @@ pcd.points = o3d.utility.Vector3dVector(points)
 
 
 # Create a voxel grid from the point cloud with a voxel_size of 0.01
-voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.001)
+voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.0001)
 
 # Initialize a visualizer object
 vis = o3d.visualization.Visualizer()
